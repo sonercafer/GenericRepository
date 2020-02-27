981632d4d882d31399c41ec20011288251c7ca8f
sonercafer <48931056+sonercafer@users.noreply.github.com>
2020-02-27 11:39:41 +0300

    Create GenericRepository.cs

---
+++ GenericRepository.cs
@@ -0,0 +1,43 @@
+public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+            where TEntity : class, IEntity
+    {
+        private AkosContext _dbContext;
+        private DbSet<TEntity> _dbSet;
+
+        public GenericRepository(AkosContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<TEntity>();
+        }
+
+        #region CRUD
+        public IList<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public TEntity GetById(string rowid)
+        {
+            return _dbSet.Find(rowid);
+        }
+
+        public void Create(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(string rowid, TEntity entity)
+        {
+            _dbSet.AddOrUpdate(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(string rowid)
+        {
+            var entity = _dbSet.Find(rowid);
+            _dbSet.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+        #endregion
+    }
